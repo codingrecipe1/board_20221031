0e6d01f6c19333b2f9433f0ea6a174202c3622ee
codingrecipe1 <codingrecipe1@gmail.com>
2022-11-01 15:39:40 +0900

    07_글조회 기능 완료

---
+++ src/main/java/com/its/board/controller/BoardController.java
@@ -0,0 +1,53 @@
+package com.its.board.controller;
+
+import com.its.board.dto.BoardDTO;
+import com.its.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @Autowired
+    private BoardService boardService;
+
+    //    @GetMapping("/board/save") // @RequestMapping x
+    @GetMapping("/save")
+    public String saveForm() {
+//        return "boardSave"; // => views/boardSave.jsp (x)
+        return "boardPages/boardSave"; // => views/boardPages/boardSave.jsp
+    }
+
+    //    @PostMapping("/board/save") /board/board/save 주소요청에 반응
+//    @PostMapping("/save")
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        boolean result = boardService.save(boardDTO);
+        if (result) {
+            return "redirect:/board/";
+        } else {
+            return "boardPages/saveFail";
+        }
+    }
+
+    @GetMapping("/")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList", boardDTOList);
+        return "boardPages/boardList";
+    }
+
+    // 상세조회: /board?id=
+    @GetMapping
+    public String findById(@RequestParam("id") Long id, Model model) {
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "boardPages/boardDetail";
+    }
+
+
+}
---
+++ src/main/java/com/its/board/service/BoardService.java
@@ -0,0 +1,35 @@
+package com.its.board.service;
+
+import com.its.board.dto.BoardDTO;
+import com.its.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardService {
+    @Autowired
+    private BoardRepository boardRepository;
+    public boolean save(BoardDTO boardDTO) {
+        int result = boardRepository.save(boardDTO);
+        if (result > 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    public List<BoardDTO> findAll() {
+        List<BoardDTO> boardDTOList = boardRepository.findAll();
+        return boardDTOList;
+    }
+
+    public BoardDTO findById(Long id) {
+        // 조회수 증가
+        boardRepository.updateHits(id);
+        // 상세내용 가져와서 리턴
+        return boardRepository.findById(id);
+    }
+}
